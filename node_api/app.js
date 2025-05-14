98322fdeb8b97ba2921fa3b860a19afb3c78e9fd
GUILHERME RAMOS DE OLIVEIRA <goliveira04@cs.up.edu.br>
2025-05-13 21:55:49 -0300

    aula06 parte 2

---
+++ node_api/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const host = '0.0.0.0'
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+
+app.listen(port, host, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
